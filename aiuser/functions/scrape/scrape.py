b8928d3f18f101ca335187185ddd40bd33de5950
karutoil <32721657+karutoil@users.noreply.github.com>
2024-08-19 21:01:48 -0400

    Update scrape.py

--- aiuser/functions/scrape/scrape.py
+++ aiuser/functions/scrape/scrape.py
@@ -9,7 +9,7 @@ logger = logging.getLogger("red.bz_cogs.aiuser")
 async def scrape_page(link: str):
     headers = {
         "Cache-Control": "no-cache",
-        "Referer": "https://www.google.com/",
+        "Referer": "https://beta.civilhost.net/",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
     }
 
